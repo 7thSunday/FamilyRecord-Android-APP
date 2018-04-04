4d1756c7682c439a7f1928e1f36e13876085bc83
REDCROW-WS\Administrator <1064578256@qq.com>
2018-04-04 14:39:55 +0800

    Revert "Revert "调用系统下载，视频模块接入""
    This reverts commit 5027a4102dca970142b21cf1041a6c98f17dfd6d.

--- app/src/main/java/com/example/administrator/familyrecord/subHomePage/VideoFragment.java
+++ app/src/main/java/com/example/administrator/familyrecord/subHomePage/VideoFragment.java
@@ -1,14 +1,41 @@
 package com.example.administrator.familyrecord.subHomePage;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.administrator.familyrecord.R;
+import com.example.administrator.familyrecord.album.ShowAlbum;
+import com.example.administrator.familyrecord.utils.ConfigUtils;
+import com.example.administrator.familyrecord.utils.GetPath;
+import com.example.administrator.familyrecord.utils.HttpUtils;
+import com.example.administrator.familyrecord.utils.RecyclerViewUtil;
+import com.example.administrator.familyrecord.video.PlayVideo;
+import com.loopj.android.image.SmartImageView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import static android.content.Context.MODE_PRIVATE;
 
 
 /**
@@ -24,6 +51,13 @@ public class VideoFragment extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+
+    public JSONObject object;
+    private RecyclerView recyclerView;
+    private Context context;
+    public ArrayList<Map<String, Object>> list=new ArrayList<Map<String,Object>>();
+    public RecyclerViewAdapter mAdapter;
+    private  String srcPath;
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -67,6 +101,222 @@ public class VideoFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_video, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        init();
+
+        final SwipeRefreshLayout mRefreshLayout = (SwipeRefreshLayout) getView().findViewById(R.id.video_swipe_refresh);
+        mRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                init();
+                mRefreshLayout.setRefreshing(false);
+            }
+        });
+
+        RecyclerViewUtil util = new RecyclerViewUtil(getActivity(),recyclerView);
+        util.setOnItemClickListener(new RecyclerViewUtil.OnItemClickListener() {
+            @Override
+            public void onItemClick(int position, View view) {
+                Intent intent = new Intent(getActivity(), PlayVideo.class);
+                intent.putExtra("path",list.get(position).get("filePath").toString());
+                startActivity(intent);
+            }
+        });
+
+        FloatingActionButton uploadVideo = (FloatingActionButton) getView().findViewById(R.id.btn_video_upload);
+        FloatingActionButton selectVideo = (FloatingActionButton) getView().findViewById(R.id.btn_select_video);
+        uploadVideo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final SharedPreferences sp=getActivity().getSharedPreferences("login",MODE_PRIVATE);
+                final String creator = sp.getString("username","");
+                final String id = sp.getString("groupId","");
+
+
+                final JSONObject user = new JSONObject();
+                try {
+                    user.put("type","2");
+                    user.put("creator",creator);
+                    user.put("rId",id);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+                if(srcPath==null){
+                    Toast.makeText(getActivity(), "请选择一段视频", Toast.LENGTH_SHORT).show();
+                }else {
+                    new Thread(){
+                        public void run(){
+
+                            HttpUtils hu = new HttpUtils();
+                            String uploadUrl = ConfigUtils.getProperties(getActivity().getApplicationContext(), "uploadUrl");
+                            String url = ConfigUtils.getProperties(getActivity().getApplicationContext(),"host") + uploadUrl;
+
+                            hu.upload(url,srcPath,user);
+
+                        }
+
+                    }.start();
+                }
+            }
+        });
+        selectVideo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent local = new Intent();
+                local.setType("video/*");
+                local.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(local, 3);
+            }
+        });
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == Activity.RESULT_OK){
+
+            Uri uri = data.getData();
+            GetPath path = new GetPath();
+            srcPath = path.getPath(getActivity().getApplicationContext(),uri);
+
+
+        }
+    }
+
+    private void init() {
+        list.clear();
+
+        context = getActivity().getApplicationContext();
+        recyclerView = (RecyclerView) getView().findViewById(R.id.recycler_view_video);
+        //使recyclerview保持固定的大小
+        recyclerView.setHasFixedSize(true);
+
+        //设置布局管理器，实现横向或数值滚动的列表布局
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(context));
+
+        //ArrayList集合
+
+        Thread getVideo = new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+                SharedPreferences sp = getActivity().getSharedPreferences("login", MODE_PRIVATE);
+                String groupId = sp.getString("groupId", null);
+                JSONObject user = new JSONObject();
+                try {
+                    user.put("rId", groupId);
+                    user.put("type","2");
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+                String selectVideoUrl = ConfigUtils.getProperties(getActivity().getApplicationContext(), "selectVideoUrl");
+                String url = ConfigUtils.getProperties(getActivity().getApplicationContext(), "host") + selectVideoUrl;
+                HttpUtils hu = new HttpUtils();
+                jsonJX(hu.selectVideo(url, user));
+
+
+            }
+
+            private void jsonJX(JSONArray data) {
+                //判断数据是空
+                if (data != null) {
+                    try {
+                        //遍历
+                        for (int i = 0; i < data.length(); i++) {
+                            object = data.getJSONObject(i);
+
+                            Map<String, Object> map = new HashMap<String, Object>();
+
+                            try {
+                                //获取到json数据中的内容
+                                String videoPath = object.getString("filePath");
+//                                String id = object.getString("id");
+//                                int type = object.getInt("type");
+                                String videoName = object.getString("fileName");
+                                //存入map
+                                map.put("filePath", videoPath);
+                                map.put("fileName", videoName);
+//                                map.put("id",id);
+//                                map.put("type",type);
+
+                                //ArrayList集合
+                                list.add(map);
+
+                            } catch (JSONException e) {
+                                // TODO Auto-generated catch block
+                                e.printStackTrace();
+                            }
+
+                        }
+
+//                        Message message = new Message();
+//                        message.what = 1;
+//                        handler.sendMessage(message);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+            }
+
+        });
+        getVideo.start();
+        try {
+            getVideo.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        mAdapter = new RecyclerViewAdapter(list);
+        recyclerView.setAdapter(mAdapter);
+
+
+    }
+
+    public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.myViewHolder>{
+
+        private ArrayList data;
+
+        public RecyclerViewAdapter(ArrayList<Map<String, Object>> list) {
+            this.data = list;
+        }
+
+        @Override
+        public myViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+            myViewHolder holder=new myViewHolder(View.inflate(viewGroup.getContext(), R.layout.album_item, null));
+            return holder;
+        }
+        //绑定数据
+
+        @Override
+        public void  onBindViewHolder(myViewHolder holder, int i) {
+
+//            list.get(i).get("albumCover").toString();
+            holder.videoName.setText(list.get(i).get("fileName").toString());
+            holder.albumCover.setImageResource(R.drawable.video);
+        }
+        @Override
+        public int getItemCount() {
+            return list.size();
+        }
+        //ViewHolder类
+        public class myViewHolder extends RecyclerView.ViewHolder{
+            TextView videoName;
+            SmartImageView albumCover;
+            public myViewHolder(View itemView)
+            {
+                super(itemView);
+                videoName = (TextView)itemView.findViewById(R.id.album_name);
+                albumCover = (SmartImageView) itemView.findViewById(R.id.album_cover);
+            }
+        }
     }
 
     // TODO: Rename method, update argument and hook method into UI event
